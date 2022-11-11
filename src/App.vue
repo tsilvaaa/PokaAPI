84e033afa8af6d18fa6a74ceef710623cd159747
tsilvaaa <117957376+tsilvaaa@users.noreply.github.com>
2022-11-11 18:52:32 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <pokemon />
+  </div>
+</template>
+
+<script>
+import Pokemon from "./components/Pokemon.vue";
+
+export default {
+  name: "app",
+  components: {
+    Pokemon,
+  },
+};
+</script>
+
+<style>
+@import url("https://use.fontawesome.com/releases/v5.8.2/css/all.css");
+
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  background: radial-gradient(#634904, #570202);
+}
+</style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container">
+    <img
+      alt="Vue logo"
+      src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg"
+    />
+    <h1>PokeAPI VueJS</h1>
+    <PokemonSearch :apiUrl="apiUrl" @setPokemonUrl="setPokemonUrl" />
+    <PokemonList
+      :imageUrl="imageUrl"
+      :apiUrl="apiUrl"
+      @setPokemonUrl="setPokemonUrl"
+    />
+    <PokemonDetail
+      v-if="showDetail"
+      :pokemonUrl="pokemonUrl"
+      :imageUrl="imageUrl"
+      @closeDetail="closeDetail"
+    />
+  </div>
+</template>
+<script>
+import PokemonSearch from "./PokemonSearch.vue";
+import PokemonList from "./PokemonList.vue";
+import PokemonDetail from "./PokemonDetail.vue";
+
+export default {
+  data: () => {
+    return {
+      imageUrl:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",
+      apiUrl: "https://pokeapi.co/api/v2/pokemon/",
+      pokemonUrl: "",
+      showDetail: false,
+    };
+  },
+  components: {
+    PokemonSearch,
+    PokemonList,
+    PokemonDetail,
+  },
+  methods: {
+    setPokemonUrl(url) {
+      this.pokemonUrl = url;
+      this.showDetail = true;
+    },
+    closeDetail() {
+      this.pokemonUrl = "";
+      this.showDetail = false;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 10px;
+  width: calc(100% - 20px);
+  min-height: calc(100vh - 20px);
+  //background: radial-gradient(#ffbf0b, #e20000);
+
+  font-family: "Acme", arial;
+  font-size: 1rem;
+  font-weight: normal;
+}
+
+h1 {
+  color: #efefef;
+}
+</style>
